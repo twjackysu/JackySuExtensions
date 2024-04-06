f93c7b917bb97230747337e0694349f30642279d
Jacky Su <jacky19918@gmail.com>
2024-04-06 17:14:58 +0800

    chore: change to support net6.0;net8.0, upgrade packages

---
+++ JackySuExtensions/ServiceCollectionExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿// using Microsoft.Extensions.DependencyInjection;
+
+namespace JackySuExtensions.ServiceCollectionExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        //public static IServiceCollection AddOOXX(this IServiceCollection services)
+        //{
+        //    // services.AddScoped<YourService>();
+
+        //    return services;
+        //}
+    }
+}
--- JackySuExtensions/StringExtensions/StringCipherExtensions.cs
+++ JackySuExtensions/StringExtensions/StringCipherExtensions.cs
@@ -27,10 +27,10 @@ namespace JackySuExtensions.StringExtensions
             var saltStringBytes = Generate128BitsOfRandomEntropy();
             var ivStringBytes = Generate128BitsOfRandomEntropy();
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations, HashAlgorithmName.SHA256))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
+                using (var symmetricKey = Aes.Create())
                 {
                     symmetricKey.BlockSize = 128;
                     symmetricKey.Mode = CipherMode.CBC;
@@ -74,10 +74,10 @@ namespace JackySuExtensions.StringExtensions
             // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
             var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
 
-            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations, HashAlgorithmName.SHA256))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
+                using (var symmetricKey = Aes.Create())
                 {
                     symmetricKey.BlockSize = 128;
                     symmetricKey.Mode = CipherMode.CBC;
@@ -88,11 +88,10 @@ namespace JackySuExtensions.StringExtensions
                         {
                             using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
                             {
-                                var plainTextBytes = new byte[cipherTextBytes.Length];
-                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                using (var plainTextReader = new StreamReader(cryptoStream))
+                                {
+                                    return plainTextReader.ReadToEnd();
+                                }
                             }
                         }
                     }
@@ -103,7 +102,7 @@ namespace JackySuExtensions.StringExtensions
         private static byte[] Generate128BitsOfRandomEntropy()
         {
             var randomBytes = new byte[16]; // 16 Bytes will give us 128 bits.
-            using (var rngCsp = new RNGCryptoServiceProvider())
+            using (var rngCsp = RandomNumberGenerator.Create())
             {
                 // Fill the array with cryptographically secure random bytes.
                 rngCsp.GetBytes(randomBytes);
